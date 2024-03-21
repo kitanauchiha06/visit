a2f321c0489798f4da901228790b67691877ef1c
karlo <karlo@DESKTOP-8P5I5KT>
2024-03-21 15:08:58 +0300

    Добавьте файлы проекта.

---
+++ visit/visit.py
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Сайт-визитка разработчика">
+    <title>Сайт-визитка</title>
+</head>
+<body>
+    <div class="header">
+        <h2>
+            <span style="font-family: Arial; color:slategray;">
+                Сайт-визитка разработчика
+            </span>
+        </h2>
+        <p>
+            <span style="font-family: Arial;">
+                ГБПОУ ВПК им. В.И. Вернадского
+            </span>
+        </p>
+    </div>
+    <hr>
+    <div class="content">
+        <img src="img/gDFs5yQU7dI.jpg" height="200px" title="Это я">
+        <h2>
+            <span style="font-family: Arial; color:slategray;">
+                Разработчик ПО Журавлева Т.А.
+            </span>
+        </h2>
+        <p>
+            <span style="font-family: Arial;">
+                Занимаюсь разработкой ПО разного направления
+            </span>
+        </p>
+        <p>
+            <span style="font-family: Arial;">
+                Специализируюсь на языках программирования Python
+            </span>
+        </p>
+    </div>
+    <div class="contact">
+        <center>
+            <h2>
+                <span style="font-family: Arial; color:slategray;">
+                    Связаться со мной можно по следующим QR-кодам:
+                </span>
+            </h2>
+                <img src="img/tg.jpg" height="200px">
+            </a>
+            <br>
+                <img src="img/vk.jpg" height="200px">
+            </a>
+        </center>
+    </div>
+    <footer>
+        <center>
+            <hr>
+            <p>
+                <span style="font-family: Arial; color:slategray;">
+                    Сайт-визитка
+                </span>
+            </p>
+            <p>
+                <span style="font-family: Arial; color:slategray;">
+                    Сайт разработал Журавлева Т.А.
+                </span>
+            </p>
+            <p>
+                <span style="font-family: Arial; color:slategray;">
+                    2024 г.
+                </span>
+            </p>
+        </center>
+    </footer>
+</body>
+</html>
